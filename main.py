b9c049c4700388870821b3cff873ede812d45a58
RubyForged <6707504+RubyForged@users.noreply.github.com>
2026-08-22 19:51:13 -0700

    CH:3 L:2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 from logger import log_state
+from player import Player
 import pygame
 
 
@@ -12,6 +13,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player1 = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # gameloop
     while True:
@@ -20,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     # end gameloop
