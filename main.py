9ca5ff2eb7bf04d7229f249b2b87950ef147820e
RubyForged <6707504+RubyForged@users.noreply.github.com>
2026-08-25 19:02:13 -0700

    CH:4 L:1 Asteroids

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from logger import log_state
 from player import Player
 import pygame
+
 
 
 def main():
@@ -16,10 +19,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     Player.containers = (updatable, drawable)
     player1 = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
 
 
 
